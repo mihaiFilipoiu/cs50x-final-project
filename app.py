ffd19834c007163f7b9c507c936dc59b8e3249cf
mihaiFilipoiu <mihai.filipoiu@gmail.com>
2024-02-26 18:10:29 +0200

    register

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-# db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///workouts.db")
 
 @app.after_request
 def after_request(response):
@@ -36,7 +36,9 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        return apology("TODO", 400)
+        # Check if "username" is empty
+        if not request.form.get("username"):
+            return apology("username field empty", 403)
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
@@ -57,7 +59,39 @@ def register():
     """Register user"""
 
     if request.method == "POST":
-        return apology("TODO", 400)
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Check if "username" is empty
+        if not username:
+            return apology("username field empty", 403)
+        # Check if "password" is empty
+        elif not password:
+            return apology("password field empty", 403)
+        # Check if "confirm" is empty
+        elif not request.form.get("confirm"):
+            return apology("pass confirmation field empty", 403)
+        
+        # Get the list of all usernames from the db
+        usernames = db.execute("SELECT username FROM users")
+
+        # Check if the username is not already used
+        for name in usernames:
+            if name["username"] == username:
+                return apology("username already used", 403)
+            
+        # Password == Pass confirmation
+        if password != request.form.get("confirm"):
+            return apology("password != confirmation", 403)
+
+        # Generate password hash
+        hash = generate_password_hash(password, method='pbkdf2', salt_length=16)
+
+        # Add the credentials to the db
+        db.execute("INSERT INTO users (username, hash) VALUES (?,?)", username, hash) 
+
+        return redirect("/login")
     else:
         return render_template("register.html")
     
@@ -68,10 +102,10 @@ def index():
 
 @app.route("/add_exercise")
 @login_required
-def index():
+def add_exercise():
     return apology("TODO", 400)
 
 @app.route("/add_log")
 @login_required
-def index():
+def add_log():
     return apology("TODO", 400)
